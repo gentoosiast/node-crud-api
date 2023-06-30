d029b0c42f8ce10b865f710bdfc2be9fc600b1a0
Sergey Koksharov <gentoosiastic@gmail.com>
2023-06-30 11:20:31 +0300

    feat: add basic types which will be used in the app

---
+++ src/types/http.ts
@@ -0,0 +1,15 @@
+export enum HTTPMethod {
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+  DELETE = 'DELETE',
+}
+
+export enum HTTPStatusCode {
+  OK = 200,
+  Created = 201,
+  NoContent = 204,
+  BadRequest = 400,
+  NotFound = 404,
+  ServerError = 500,
+}
