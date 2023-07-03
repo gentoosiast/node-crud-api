87c9033c0008644a6e05ba638710dc20a619c8f0
Sergey Koksharov <gentoosiastic@gmail.com>
2023-07-03 20:12:55 +0300

    fix: provide consistency when multiple single servers are created

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import { startCluster } from './multi.js';
 import { startHTTPServer } from './server.js';
-import { dispatcher } from './dispatcher.js';
+import { createDispatcher } from './dispatcher.js';
 import { isMultiMode } from './helpers/cli.js';
 import { DEFAULT_SERVER_PORT, SERVER_HOSTNAME } from './constants/index.js';
 
@@ -12,6 +12,6 @@ if (isMultiMode()) {
   await startCluster(SERVER_HOSTNAME, PORT);
   console.log('Cluster is fully operational and ready to receive requests');
 } else {
-  await startHTTPServer(SERVER_HOSTNAME, PORT, dispatcher);
+  await startHTTPServer(SERVER_HOSTNAME, PORT, createDispatcher());
   console.log('Server is fully operational and ready to receive requests');
 }
